66a5f50b66a5a52c90c07f4ba294ad7e5046f314
Andressa Da Costa <andressadacostan@gmail.com>
2022-12-07 14:43:58 -0300

    Absolute Imports

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,10 +1,10 @@
 import banner from './banner.png';
-import Cabecalho from '../../componentes/Cabecalho';
-import Menu from '../../componentes/Menu';
+import Cabecalho from 'componentes/Cabecalho';
+import Menu from 'componentes/Menu';
 import styles from './PaginaInicial.module.scss';
-import Populares from '../../componentes/Populares';
-import Galeria from '../../componentes/Galeria';
-import Rodape from '../../componentes/Rodape';
+import Populares from 'componentes/Populares';
+import Galeria from 'componentes/Galeria';
+import Rodape from 'componentes/Rodape';
 
 export default function PaginaInicial() {
 	return (
